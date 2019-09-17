301c970df8b52e07400b0b84ff2eb1aa84de401f
Ahmed Amin <ahmed.mo.amin@outlook.com>
2019-09-17 17:46:13 +0200

    add buyinvoice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ export const Approutes: Routes = [
       { path: 'Maininvoice', loadChildren: './invoices/invoices.module#InvoicesModule', canActivate: [AuthGuard] },
       { path: 'addinvoice', loadChildren: './invoices/addinvoice/addinvoice.module#AddinvoiceModule', canActivate: [AuthGuard] },
       { path: 'viewinvoice', loadChildren: './invoices/viewinvoice/viewinvoice.module#ViewinvoiceModule', canActivate: [AuthGuard] },
-
+      { path: 'buyinvoice', loadChildren: './invoices/buyinvoice/buyinvoice.module#BuyinvoiceModule', canActivate: [AuthGuard] },
       // LAZY LOAD PENDING ..
       //   {   path:'Maininvoice',    
 
---
+++ src/app/invoices/buyinvoice/buyinvoice.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import { HttpClient } from "@angular/common/http";
+
+interface Buyinvoice {
+  ID: number;
+  NameAr: string;
+  NameEn: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BuyinvoiceServices {
+  constructor(private http: HttpClient) { }
+
+  getProduct(value, callback) {
+    return this.http.post(`https://api.oclinico.com/PharmacyAPI/api/buy-invoice/get-product/${value}`, {})
+      .subscribe((res) => {
+        callback(res);
+      });
+  }
+
+  saveOrder(data, callback) {
+    return this.http.post(`https://api.oclinico.com/PharmacyAPI/api/buy-invoice/add-new-buy-invoice`, data, {})
+      .subscribe((res) => {
+        callback(res);
+      });
+  }
+
+  getMaster(callback) {
+    return this.http.post(`https://api.oclinico.com/PharmacyAPI/api/buy-invoice/get-store-manufacturer`, {})
+      .subscribe((res) => {
+        callback(res);
+      });
+  }
+
+}
+
